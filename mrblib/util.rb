bd667f615aa5884068aa96a1962b5da8cabacf27
Thiago Scalone <thiago@scalone.com.br>
2017-03-15 22:05:17 -0300

    Implement Util.ask and Util.ask_secret.

--- mrblib/util.rb
+++ mrblib/util.rb
@@ -4,6 +4,12 @@ module Util
       w.split("_").map {|w2| w2.capitalize }.join('')
     }.join('')
   end
+
+  def ask(str)
+    `read -p "#{str}" value; echo $value`
   end
 
+  def ask_secret(str)
+    `read -s -p "#{str}" value; echo $value`
+  end
 end
