68eefd9d7a84491267206a4c7bdcefb41bbed209
Thiago Scalone <thiago@scalone.com.br>
2017-08-18 22:18:01 -0300

    Mode xml to posxml method to helper.

--- lib/cloudwalk/manager_helper.rb
+++ lib/cloudwalk/manager_helper.rb
@@ -11,5 +11,14 @@ module Cloudwalk
     def host
       Cloudwalk::Config.host
     end
+
+    def posxml2xml(str)
+      str.sub(".posxml", ".xml")
+    end
+
+    def xml2posxml(str)
+      str.sub(".xml", ".posxml")
+    end
   end
 end
+
