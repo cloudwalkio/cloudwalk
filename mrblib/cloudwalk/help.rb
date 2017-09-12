de77936d1c0f5f1689c43975344db2b15dfed3ff
Thiago Scalone <thiago@scalone.com.br>
2017-09-12 15:09:25 -0300

    Update help with new commands (app and release)

--- mrblib/cloudwalk/help.rb
+++ mrblib/cloudwalk/help.rb
@@ -1,17 +1,19 @@
 module Cloudwalk
   class Help
     def self.run
-      puts "cloudwalk [switches] [arguments]"
+      puts "cloudwalk [command] [arguments]"
       puts "cloudwalk help                                   : show this message"
+      puts "cloudwalk sign-up                                : perform sign-up process"
       puts "cloudwalk login                                  : perform cloudwalk.io authentication"
       puts "cloudwalk logout                                 : perform cloudwalk.io logoff"
       puts "cloudwalk new <name>                             : create app, with: -xml to xml app"
       puts "cloudwalk compile -o<out> <file1> <file2>        : compile ruby to mrb"
-      puts "cloudwalk app upgrade-version <name> <old>..<new>: create a new version for xml app"
-      puts "cloudwalk console                                : mirb console"
       puts "cloudwalk run -b <out>, <file1>                  : run ruby or binary file"
-      puts "cloudwalk package <parameters>                   : Create package based in the params: MODEL, BRAND, SERIAL_NUMBER, LOGICAL_NUMBER; ie: DEVICE=d200"
-      puts "cloudwalk config <parameter>                     : print config parameters (token, user_id, env, host)"
+      puts "cloudwalk app                                    : arguments list or new"
+      puts "cloudwalk console                                : mirb console"
+      puts "cloudwalk release <argument>                     : manipulate releases based in Cwfile.json"
+      puts "cloudwalk package <arguments>                    : create package based in the params: MODEL, BRAND, SERIAL_NUMBER, LOGICAL_NUMBER; ie: DEVICE=d200"
+      puts "cloudwalk config <argument>                      : print config parameters (token, user_id, env, host)"
       puts "cloudwalk about                                  : print cloudwalk cli about"
       puts "cloudwalk version                                : print cloudwalk version"
     end
