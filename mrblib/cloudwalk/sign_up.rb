ed8af12ca1fb0d0e4a185b41583424b278f38301
Thiago Scalone <thiago@scalone.com.br>
2017-09-12 15:07:41 -0300

    Implement sign-up command

---
+++ mrblib/cloudwalk/sign_up.rb
@@ -0,0 +1,21 @@
+module Cloudwalk
+  class SignUp
+    def self.run(*args)
+      puts "Welcome!"
+      puts "You're about to create a new cloudwalk account."
+      puts "Please answer the following questions..."
+
+      name  = Util.ask("Your name (optional): ")
+      email = Util.ask("Your e-mail: ")
+      pass  = Util.ask_secret("Your password: ")
+      puts ""
+      ret, err = Manager::User.sign_up(name, email, pass)
+      if ret
+        puts "Please check confirmation e-mail before execute cloudwalk login"
+      else
+        puts "Problem to sign-up: #{err}"
+      end
+    end
+  end
+end
+
