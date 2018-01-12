cd921cab482bbcd2ff4c8ab421a2ef079219ce4e
Thiago Scalone <thiago@scalone.com.br>
2018-01-12 15:15:59 -0200

    Add translation (xml to ruby via posxml_parser) option on compilation switch with the flag -txml

--- mrblib/util/posxml.rb
+++ mrblib/util/posxml.rb
@@ -5,6 +5,12 @@ module Util
       posxml = PosxmlCompiler.compile(source, Util::POSXML_EN_XSD)
       File.open(out, "w") {|f| f.write(posxml) }
     end
+
+    def self.translate(source, out)
+      Util::DaFunkRuntime.setup
+      ruby = PosxmlTranslate.translate(source, Util::POSXML_EN_XSD)
+      File.open(out, "w") {|f| f.write(ruby) }
+    end
   end
 end
 
