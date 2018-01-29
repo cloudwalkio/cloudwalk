63c92a985d110c69dfa3945dcae73828735c8d96
Thiago Scalone <thiago@scalone.com.br>
2018-01-29 18:11:16 -0200

    Implement Manager::RubyApplication.delete

--- mrblib/manager/ruby_application.rb
+++ mrblib/manager/ruby_application.rb
@@ -26,6 +26,20 @@ module Manager
       application["ruby_app"] if application
     end
 
+    def self.delete(ruby_app)
+      if ruby_app || ruby_app["id"]
+        url = "#{Cloudwalk::Config.host}/v1/apps/ruby/#{ruby_app["id"]}?access_token=#{self.token}"
+        response = Util::CloudwalkHttp.delete(url, {})
+        if response.code == 200 || response.code == 204
+          [true]
+        else
+          [false, response.body]
+        end
+      else
+        [false, "Id not found"]
+      end
+    end
+
     def self.token
       Cloudwalk::Config.token
     end
