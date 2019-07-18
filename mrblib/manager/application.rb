78a06607f8587356494c21e3f175ebf731f4cfee
danilo <danilo.maldito@gmail.com>
2019-07-18 17:22:56 -0300

    Refactored self.all, now parse right the new json returned by the manager.

--- mrblib/manager/application.rb
+++ mrblib/manager/application.rb
@@ -1,18 +1,18 @@
 module Manager
   class Application
     def self.all
-      url = "#{Cloudwalk::Config.host}/v1/apps/posxml?access_token=#{self.token}&per_page=100"
+      url = "#{Cloudwalk::Config.host}/v1/apps?access_token=#{self.token}&per_page=100"
       response = Util::CloudwalkHttp.get(url, {})
       if response.code == 200
         json = JSON.parse(response.body)
         total_pages = json["pagination"]["total_pages"].to_i
-        apps = json["posxmlapps"]
+        apps = json["apps"]
 
         (total_pages - 1).times do |page|
-          url = "#{Cloudwalk::Config.host}/v1/apps/posxml?access_token=#{self.token}&per_page=100&page=#{page+2}"
+          url = "#{Cloudwalk::Config.host}/v1/apps?access_token=#{self.token}&per_page=100&page=#{page+2}"
           response = Util::CloudwalkHttp.get(url, {})
           if response.code == 200
-            apps.concat(JSON.parse(response.body)["posxmlapps"])
+            apps.concat(JSON.parse(response.body)["apps"])
           end
         end
       end
